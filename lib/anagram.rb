7a7c21655726072233e1e4107476f2bfc9c953c9
matthew enbar <matthew.enbar@gmail.com>
2020-04-04 14:35:00 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+
+
+def initialize(word)
+  @word = word
+
+#takes an array of possible anagrams.
+#It should return all matches in an array.
+#If no matches exist, it should return an empty array.
+def match
+
+end
+
+
+
+
+
+end
