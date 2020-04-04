2edced374731e37a0662fe093bb02428c412b508
matthew enbar <matthew.enbar@gmail.com>
2020-04-04 14:43:04 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,8 +13,14 @@ class Anagram
   #It should return all matches in an array.
   #If no matches exist, it should return an empty array.
   def match(anagram_array)
+    array = []
     word = word.split.sort
-
+    anagram_array.each do |element|
+      if element.split.sort == word
+        array << element
+      end
+    end
+    array
   end
 
 
