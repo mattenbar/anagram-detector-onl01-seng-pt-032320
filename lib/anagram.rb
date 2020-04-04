620f0c9761237dc555b8d67c956335d5e2077c76
matthew enbar <matthew.enbar@gmail.com>
2020-04-04 14:35:38 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,16 +5,16 @@ class Anagram
 
 
 
-def initialize(word)
-  @word = word
-end
+  def initialize(word)
+    @word = word
+  end
 
-#takes an array of possible anagrams.
-#It should return all matches in an array.
-#If no matches exist, it should return an empty array.
-def match
+  #takes an array of possible anagrams.
+  #It should return all matches in an array.
+  #If no matches exist, it should return an empty array.
+  def match
 
-end
+  end
 
 
 
